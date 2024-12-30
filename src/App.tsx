f4233e1ee9c9b23fb9640ce1aa33b0f8c000f3cb
Gustavo Emanuel <gustavoe.dev@gmail.com>
2024-12-30 13:15:26 -0300

    add tailwind css configuration; implement utility functions for class management

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import "./global.css";
+
 export function App() {
   return (
-    <div>
+    <div className="flex justify-center items-center h-screen">
       <h1>Hello World</h1>
     </div>
   );
